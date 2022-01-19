1afb75766041877f1078ae9041f4c2f15d95bd32
roy <Roy Yanez>
2022-01-18 20:12:36 -0600

    template logic 50% done

--- examples.js
+++ examples.js
@@ -1,54 +1,83 @@
 import {useEffect, useState} from "react";
+import {Text, TextInput, TouchableOpacity, View} from "react-native";
 
-const [name, setName] = useState('');
+export default function Examples() {
 
-//Save data
-const save = async()=> {
-    try {
-        await AsyncStorage.setItem('MyName', name)
-    }catch (err) {
-        alert(err);
-    }
+    const [value, setValue] = useState('');
 
-    // let template = {
-    //     name: "Template name",
-    //     location: "US"
-    // };
-    // await AsyncStorage.setItem("MyName", JSON.stringify(template));
-};
-
-
-
-//Load data
-const load = async()=> {
-    try {
-        let name = await AsyncStorage.getItem('MyName')
-
-        if (name !== null) {
-            setName(name)
+    //Add Data
+    const setStringValue = async () => {
+        try {
+            if (value === '') {
+                alert("EMPTY VALUE IDIOT")
+            } else {
+                await AsyncStorage.setItem('Template name:', value)
+            }
+        } catch (e) {
+            alert("SOME STUPID ERROR")
         }
-    }catch (err) {
-        alert(err);
+        console.log('Done.')
+    };
+
+    //Get data
+    const getAllKeys = async () => {
+        let keys = []
+        try {
+            keys = await AsyncStorage.getAllKeys()
+            alert("Done")
+            console.log(keys)
+        } catch (err) {
+            alert("NOPE")
+        }
+    };
+
+    //Get Item
+    const getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('Template name:')
+            if(value !== null) {
+                // value previously stored
+            }
+            console.log(value)
+        } catch(e) {
+            // error reading value
+        }
     }
 
-    // let jsonValue = await AsyncStorage.getItem('MyName')
-    //
-    // if (jsonValue !=null) {}
-    // setName(JSON.parse(jsonValue))
 
+    //Remove Data
+    const removeFew = async () => {
+        const keys = ['key']
+        try {
+            await AsyncStorage.multiRemove(keys)
+        } catch (e) {
+            // remove error
+        }
+        console.log('Done')
+    };
+
+
+    return <>
+
+        <TextInput onChangeText={(input) => setValue(input)}/>
+        <TouchableOpacity onPress={setStringValue}>
+            <Text>Save data</Text>
+        </TouchableOpacity>
+
+        <View>
+            <TouchableOpacity onPress={() => getAllKeys()}>
+                <Text>Get all keys</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity onPress={() => removeFew()}>
+                <Text>Remove keys</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity onPress={() => getData()}>
+                <Text>Get Item</Text>
+            </TouchableOpacity>
+        </View>
+    </>
 };
-
-useEffect(()=>{
-    load();
-}, []);
-
-//Remove data
-const remove = async ()=> {
-    try {
-        await AsyncStorage.removeItem("MyName");
-    } catch (err) {
-        alert(err);
-    } finally {
-        setName('');
-    }
-}
--- screens/create_S.tsx
+++ screens/create_S.tsx
@@ -1,72 +1,145 @@
-import {BackHandler, Button, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
-import Nav from "../components/layouts/nav";
-import {Numeric_Input, Text_Input} from "../components/templat_componets";
-import {useEffect, useState} from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+    Button, Modal,
+    Pressable,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from "react-native";
+import React, {useState} from "react";
+import tailwind from "tailwind-rn";
+import McIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FaIcons from "react-native-vector-icons/FontAwesome";
 
 
 export default function Create_S() {
+    //Modal useState
+    const [modalVisible, setModalVisible] = useState(false);
 
-    const [value, setValue] = useState('');
+    //Component useState
+    const [component, setComponent] = useState();
+    const [items, setItems] = useState([]);
 
-    //Add Data
-    const setStringValue = async () => {
 
-        try {
-            if (value === '') {
-                alert("EMPTY VALUE IDIOT")
-            } else {
-                await AsyncStorage.setItem('Template', value)
-            }
-        } catch (e) {
-            alert("SOME STUPID ERROR")
-        }
-        console.log('Done.')
+    const handleAdd = ()=> {
+        setItems([...items, component])
+        console.log("ADDED")
     }
 
-    //Get data
-    const getAllKeys = async () => {
-        let keys = []
-        try {
-            keys = await AsyncStorage.getAllKeys()
-            alert("Done")
-            console.log(keys)
-        } catch (err) {
-            alert("NOPE")
-        }
-
+    const eraseComponent = (index) => {
+        let itemsCopy = [...items];
+        itemsCopy.splice(index, 1);
+        setItems(itemsCopy)
     }
 
-    //Remove Data
-    const removeFew = async () => {
-        const keys = ['key']
-        try {
-            await AsyncStorage.multiRemove(keys)
-        } catch (e) {
-            // remove error
-        }
 
-        console.log('Done')
-    }
+
+    const CheckBox = () => {
+        return (
+            <View style={tailwind('flex-row items-center px-1')}>
+                {checkbox}
+                <TextInput
+                    style={tailwind('text-2xl text-pink-600')}
+                    placeholder={'Title: E.g. "Negotiable price"'}/>
+            </View>
+        )
+    };
+
+    const Description = () => {
+        return (
+            <View style={tailwind('flex-row items-center')}>
+                <View style={tailwind('items-center px-2')}>
+
+                    <TextInput
+                        style={tailwind('text-2xl items-center w-full text-pink-600')}
+                        placeholder={'Title: E.g. "Color"'}
+                    />
+                    <View style={tailwind('w-full h-9 bg-gray-300 border border-gray-400')}/>
+                </View>
+            </View>
+        )
+    };
 
 
     return <>
 
-        <TextInput onChangeText={(input) => setValue(input)}/>
-        <TouchableOpacity onPress={setStringValue}>
-            <Text>Save data</Text>
-        </TouchableOpacity>
+        <Modal
+            animationType={"fade"} transparent={true} visible={modalVisible}
+            onRequestClose={() => setModalVisible(!modalVisible)}
+        >
+            <View style={styles.modalScreen}>
+                <View style={styles.modal}>
+                    <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                        {close_icon}
+                    </Pressable>
+                    {/*Add CheckBox*/}
+                    <TouchableOpacity
+                        style={tailwind('m-2 flex-row items-center px-2 py-1 border border-gray-400 rounded-md')}
+                        onPress={() => { // @ts-ignore
+                            setComponent(CheckBox);
+                            handleAdd();
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        {checkBox_icon}
+                        <Text style={tailwind('text-3xl px-2 text-gray-600')}>Add CheckBox</Text>
+                    </TouchableOpacity>
+                    {/*Add Description*/}
+                    <TouchableOpacity
+                        style={tailwind('m-2 flex-row items-center px-2 py-1 border border-gray-400 rounded-md')}
+                        onPress={() => { // @ts-ignore
+                            setComponent(Description);
+                            handleAdd();
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        {description}
+                        <Text style={tailwind('text-3xl px-2 text-gray-600')}>Add Description</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
 
-        <View>
-            <TouchableOpacity onPress={() => getAllKeys()}>
-                <Text>Get all keys</Text>
+        <SafeAreaView style={tailwind('border border-red-400 h-full ')}>
+            <View style={tailwind('flex-row items-center border-b border-gray-400 p-2')}>
+                <Text>Template Title:</Text>
+                <TextInput
+                    style={tailwind('h-10 border border-gray-300')}
+                    placeholder={' E.g. "Cars", "Cellphones"'}
+                />
+            </View>
+            {/*Map array components*/}
+            {
+                items.map((item, index)=>{
+                    return (
+                        <TouchableOpacity key={index} onPress={()=> eraseComponent(index)}>
+                            {item}
+                        </TouchableOpacity>
+                    )
+                })
+            }
+            {/*Add button*/}
+            <View style={tailwind('items-center w-full py-4')}>
+                <View style={tailwind('w-3/6')}>
+                    <Button title={"+"} color={'#ff005f'}
+                            onPress={() => setModalVisible(true)}
+                    />
+                </View>
+
+            </View>
+            <TouchableOpacity
+                style={tailwind('m-2 flex-row items-center px-2 py-1 border border-gray-400 rounded-md')}
+                onPress={() => { // @ts-ignore
+                    setComponent(CheckBox);
+                    handleAdd();
+                }}
+            >
+                {checkBox_icon}
+                <Text style={tailwind('text-3xl px-2 text-gray-600')}>Add CheckBox</Text>
             </TouchableOpacity>
-        </View>
-        <View>
-            <TouchableOpacity onPress={() => removeFew()}>
-                <Text>Remove keys</Text>
-            </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     </>
 };
 
@@ -74,9 +147,29 @@ export default function Create_S() {
 
 //Tailwind Styled variables
 
-//Js Styles
 
+//Js Styles
+const styles = StyleSheet.create({
+    modalScreen: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
+    modal: {
+        width: '90%',
+        backgroundColor: 'white',
+        borderRadius: 15
+    },
+    closeIcon: {
+        fontSize: 50,
+        color: '#ff005f',
+    }
+});
 
 //Icons
 
-
+const checkbox = <McIcons name={'checkbox-blank-outline'} style={tailwind('text-gray-500 text-3xl')}/>
+const description = <McIcons name={'card-text-outline'} style={[styles.closeIcon]}/>
+const close_icon = <FaIcons name={'window-close'} style={[styles.closeIcon, tailwind('text-right bottom-1')]}/>
+const checkBox_icon = <McIcons name={'checkbox-multiple-marked-outline'} style={[styles.closeIcon]}/>
